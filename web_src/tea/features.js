68a8322c891b6923a689aabd9d9b935ecb62d5c1
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-01-30 22:14:52 +0530

    Delete features.js

--- web_src/tea/features.js
+++
@@ -1,6 +0,0 @@
-if(window.location.href == "https://pay.rahul.eu.org/?m=1"){
-      break loops;}
-  loops: if(window.location.href != "https://pay.rahul.eu.org/"){
-      alert("illegal domain found, please purchase it from telegram @paid_coddr");
-     window.location.href = 'https://pay.rahul.eu.org';
-  }
