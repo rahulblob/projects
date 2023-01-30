688c32b64d0a55ab8565b81bcbe8240794044597
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-01-30 16:21:14 +0530

    Create features.js

---
+++ web_src/tea/features.js
@@ -0,0 +1,6 @@
+if(window.location.href == "https://pay.rahul.eu.org/?m=1"){
+      break loops;}
+  loops: if(window.location.href != "https://pay.rahul.eu.org/"){
+      alert("illegal domain found, please purchase it from telegram @paid_coddr");
+     window.location.href = 'https://pay.rahul.eu.org';
+  }
