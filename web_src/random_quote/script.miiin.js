3b017309826c4ae0f71893b91f228661fa772f74
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-12 14:11:57 +0530

    Update and rename script.miin.js to script.miiin.js

--- web_src/random_quote/script.miiin.js
+++ web_src/random_quote/script.miiin.js
@@ -15,7 +15,7 @@ function saveQuote(){
 let Stitle = textarea1.innerHTML.replace("<span","").replace("id","").replace("</span>","").replace("<cite","").replace("id","").replace("=","").replace("=","").replace("</cite>","").replace('cite1">',"").replace('Qtext">',"").replace('"',"").replace('id"','').replace('"','');
 let Stect = textarea.innerHTML.replace("<cite>","").replace("</cite>","");
 const shareData = {
-  title: ,
+  title: Stitle,
   text: Stect,
   url: window.location.href
 }
