7f192ee66572d301ee57b497eebe0e46723b27da
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-12 14:16:25 +0530

    Update and rename script.miiiin.js to script.mn.js

--- web_src/random_quote/script.mn.js
+++ web_src/random_quote/script.mn.js
@@ -12,8 +12,8 @@ function saveQuote(){
     link.href = fileUrl;
     link.click();
 };
-let Stitle = textarea1.innerHTML.replace("<span","").replace("id","").replace("</span>","").replace("<cite","").replace("id","").replace("=","").replace("=","").replace("</cite>","").replace('cite1">',"").replace('Qtext">',"").replace('"',"").replace('id"','').replace('"','');
-let Stect = textarea.innerHTML.replace("<cite>","").replace("</cite>","");
+let Stitle = textarea.innerHTML.replace("<span","").replace("id","").replace("</span>","").replace("<cite","").replace("id","").replace("=","").replace("=","").replace("</cite>","").replace('cite1">',"").replace('Qtext">',"").replace('"',"").replace('id"','').replace('"','');
+let Stect = textarea1.innerHTML.replace("<cite>","").replace("</cite>","");
 const shareData = {
   title: "Quotes",
   text: Stitle,
