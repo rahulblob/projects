d77944728b3a59425b9962b28d5c60b1aca1ee5f
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-19 00:59:38 +0530

    Create buymeatea.js

---
+++ integrations/buymeatea.js
@@ -0,0 +1,2 @@
+let teaUi = document.createElement("div");teaUi.innerHTML = `<a href="https://pay.rahul.eu.org" class="teaS" referrerpolicy="origin" target="_blank"><span>☕</span> buymeatea</a>`;
+document.body.appendChild(teaUi);
