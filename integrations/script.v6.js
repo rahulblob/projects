7a51a812ce5135ee3033869cd59c337875af9d95
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-02-19 01:12:00 +0530

    Update and rename script.v5.js to script.v6.js

--- integrations/script.v6.js
+++ integrations/script.v6.js
@@ -2,7 +2,7 @@ let teaUi = document.createElement("div");teaUi.innerHTML = `<a href="https://pa
 document.body.appendChild(teaUi);
 document.querySelector(".teaS").style = 'background:#427eff;box-shadow: 0px 10px 10px rgba(0,0,0,.1);border:none;border-radius:20px;padding:10px 17px;color:#f3f3f3;font-size:18px;font-weight:400;font-family: "Inter", sans-serif;position:fixed;bottom:30px;right:20px;transition:all .05s;text-decoration:none;cursor:pointer;';
 document.querySelector(".teaS span").style = 'margin-right:3px;';
-$(".teaS").hover(function(){
+document.querySelector(".teaS").onmouseover = function(){
   this.style.transform = "scale(0.97)";
   this.style.background = "#306fff";
-});
+};
